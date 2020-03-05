871883857a0c9c44ffdda493a909f56375b87875
myno1600 <myno1600@student.miun.se>
2020-03-05 15:01:24 +0100

    kult

--- src/multiple-linear-regression/main.py
+++ src/multiple-linear-regression/main.py
@@ -9,17 +9,22 @@ from sklearn import preprocessing
 # Importing the dataset
 dataset = pd.read_csv('winequality-white.csv')
 
+dataset = dataset.merge(dataset)
+dataset = dataset.merge(dataset)
+
+
 x = dataset.values #returns a numpy array
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 dataset = pd.DataFrame(x_scaled)
 
-X = dataset.iloc[:, :-1].values
+
+X = dataset.iloc[:, [0,1,2,4,6,8,9,10]].values
 y = dataset.iloc[:, 11].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
 
 
 # Fitting Multiple Linear Regression to the Training set
@@ -29,6 +34,10 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+from sklearn.metrics import max_error
+from sklearn.metrics import mean_absolute_error
+max_error(y_test, y_pred)
+mean_absolute_error(y_test, y_pred)
 
 # Building the optimal model using Backward Elimination
 import statsmodels.api as sm
